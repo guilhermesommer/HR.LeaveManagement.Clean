2f1c6d164a26c34146cbd2cad28b9309a2553201
Guilherme Sommer <guilhermesommer@gmail.com>
2023-08-09 15:26:46 -0400

    Add project files.

---
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Contracts.Persistence;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<IReadOnlyList<T>> GetAsync();
+
+    Task<T> GetByIdAsync(int id);
+
+    Task CreateAsync(T entity);
+
+    Task UpdateAsync(T entity);
+
+    //Task<T> DeleteAsync(int id);
+    Task DeleteAsync(T entity);
+}
