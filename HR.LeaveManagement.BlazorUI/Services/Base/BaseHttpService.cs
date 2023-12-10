0119c3a270cf59b24d4176d1f8c20b58273faee8
Guilherme Sommer <guilhermesommer@gmail.com>
2023-12-10 14:12:38 -0500

    finished section 9 adding the UI

---
+++ HR.LeaveManagement.BlazorUI/Services/Base/BaseHttpService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace HR.LeaveManagement.BlazorUI.Services.Base
+{
+    public class BaseHttpService
+    {
+        protected IClient _client;
+
+        public BaseHttpService(IClient client)
+        {
+            _client = client;
+        }
+
+        protected Response<Guid> ConvertApiExceptions<Guid>(ApiException ex)
+        {
+            switch (ex.StatusCode)
+            {
+                case 400:
+                    return new Response<Guid>()
+                    {
+                        Message = "Invalid data was submitted",
+                        ValidationErrors = ex.Response,
+                        Success = false
+                    };
+
+                case 404:
+                    return new Response<Guid>()
+                    {
+                        Message = "The record was not found",
+                        Success = false
+                    };
+
+                default:
+                    return new Response<Guid>()
+                    {
+                        Message = "Something went wrong, please try again later",
+                        Success = false
+                    };
+            }
+        }
+    }
+}
